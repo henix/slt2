1084ad522fe665f3ad685d8fa6f6d39867d71c5c
henix <shellpick@gmail.com>
2016-02-16 23:32:46 +0800

    1. Add nil_empty test case #5 2. Add end_tag_missing test case, support failure test #11

--- tests.sh
+++ tests.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 for t in tests/* ; do
-  echo "## $t"
-  colordiff -U3 <(${LUA:-lua} ./runslt2.lua -d "$t/def.lua" -t "$t/tmpl" < "$t/data.lua") "$t/ans"
+  echo "## ${t#tests/}"
+
+  OPTS=()
+  if [ -f "$t/def.lua" ] ; then
+    OPTS+=(-d "$t/def.lua")
+  fi
+
+  if [ -f "$t/ans" ]; then
+    # normal test
+    colordiff -U3 <(${LUA:-lua} ./runslt2.lua "${OPTS[@]}" -t "$t/tmpl" < "$t/data.lua") "$t/ans"
+  elif [ -f "$t/err" ]; then
+    # error test
+    errout=$(mktemp)
+    ${LUA:-lua} ./runslt2.lua "${OPTS[@]}" -t "$t/tmpl" < "$t/data.lua" >/dev/null 2>"$errout"
+    if ! eval "$(< "$t/err")" < "$errout" ; then
+      cat "$errout"
+    fi
+    rm -f "$errout"
+  else
+    echo "ans or err must exists in $t"
+  fi
 done
